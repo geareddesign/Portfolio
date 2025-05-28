12703464cbe9ea02bda74cb99c5560cd306c9b33
Darrell Baker <bakerd355@gmail.com>
2025-05-28 18:09:20 -0400

    d

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       
 
   <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300">
-  Just Do it
+  something
 </button>
 
 
